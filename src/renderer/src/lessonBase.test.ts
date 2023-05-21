be507a6b5a58f4ddade56f3da1e855674b908550
Pavel Koryagin <pavel@koryagin.com>
2023-05-21 13:16:09 +0400

    Implement getWords()

--- src/renderer/src/lessonBase.test.ts
+++ src/renderer/src/lessonBase.test.ts
@@ -1,4 +1,4 @@
-import { getSentences } from './lessonsBase'
+import { getSentences, getWords } from './lessonsBase'
 
 describe('getSentences', () => {
   it('handles typical case', () => {
@@ -13,5 +13,23 @@ describe('getSentences', () => {
       'მე ვარ კორეელი სტუდენტი!',
       'ეს არის ქართველი სტუდენტი.'
     ])
+  })
+})
+
+describe('getWords', () => {
+  it('handles typical case in Georgian', () => {
+    // Act
+    const result = getWords('მე ვარ კორეელი სტუდენტი!')
+
+    // Assert
+    expect(result).toStrictEqual(['მე', 'ვარ', 'კორეელი', 'სტუდენტი'])
+  })
+
+  it('handles typical case in English', () => {
+    // Act
+    const result = getWords(`I'm at the university.`)
+
+    // Assert
+    expect(result).toStrictEqual([`I'm`, 'at', 'the', 'university'])
   })
 })

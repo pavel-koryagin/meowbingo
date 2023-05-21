1386da7cce70f533ae596e72ac9116f11bdcdfcd
Pavel Koryagin <pavel@koryagin.com>
2023-05-21 22:29:52 +0400

    Implement hint

--- src/renderer/src/TrainingPane.tsx
+++ src/renderer/src/TrainingPane.tsx
@@ -1,17 +1,20 @@
 import { acceptAnswer, getNextLesson } from './studentProgress'
 import { TrainingPaneView } from './TrainingPaneView'
 import { useState } from 'react'
+import _shuffle from 'lodash/shuffle'
 
 export function TrainingPane(): JSX.Element {
   const [isAnswerPerfect, setIsAnswerPerfect] = useState(false)
   const [showAnswer, setShowAnswer] = useState(false)
 
   const [enrichedLesson, setEnrichedLesson] = useState(getNextLesson())
+  const [hint, setHint] = useState<string[] | undefined>(undefined)
 
   return (
     <TrainingPaneView
       enrichedLesson={enrichedLesson}
       showAnswer={showAnswer}
+      hint={hint}
       isAnswerPerfect={isAnswerPerfect}
       onSubmit={(answer) => {
         // Accept
@@ -24,6 +27,13 @@ export function TrainingPane(): JSX.Element {
       onNext={() => {
         setEnrichedLesson(getNextLesson())
         setShowAnswer(false)
+        setHint(undefined)
+      }}
+      onHint={() => {
+        const words = enrichedLesson.lesson.askInGeorgian
+          ? enrichedLesson.engWords
+          : enrichedLesson.geoWords
+        setHint(_shuffle(words))
       }}
     />
   )
--- src/renderer/src/TrainingPaneView.tsx
+++ src/renderer/src/TrainingPaneView.tsx
@@ -4,9 +4,11 @@ import { createRef, useEffect, useState } from 'react'
 interface Props {
   enrichedLesson: EnrichedLesson
   showAnswer: boolean
+  hint?: string[]
   isAnswerPerfect: boolean
   onSubmit: (answer: string) => void
   onNext: () => void
+  onHint: () => void
 }
 
 export function TrainingPaneView({
@@ -14,11 +16,14 @@ export function TrainingPaneView({
     lesson: { eng, geo, askInGeorgian }
   },
   showAnswer,
+  hint,
   isAnswerPerfect,
   onSubmit,
-  onNext
+  onNext,
+  onHint
 }: Props): JSX.Element {
   const [answer, setAnswer] = useState('')
+  const [usedHints, setUsedHints] = useState<number[]>([])
   const textareaRef = createRef<HTMLTextAreaElement>()
 
   const canSubmit = answer.trim() !== ''
@@ -65,19 +70,90 @@ export function TrainingPaneView({
         />
       </div>
       <div className="form-group mb-3">
-        {showAnswer ? (
-          <div className={`alert ${isAnswerPerfect ? 'alert-success' : 'alert-warning'}`}>
-            {askInGeorgian ? eng : geo}
+        <div
+          className={`alert ${
+            !showAnswer ? 'alert-info' : isAnswerPerfect ? 'alert-success' : 'alert-warning'
+          }`}
+        >
+          {hint && !showAnswer ? (
+            <>
+              {hint.map((word, index) => (
+                <span
+                  key={index}
+                  className={`me-3 ${usedHints.includes(index) ? 'opacity-50' : ''}`}
+                  style={{
+                    cursor: 'pointer',
+                    borderBottom: '1px dashed var(--bs-info)'
+                  }}
+                  onClick={() => {
+                    setAnswer(answer.trimEnd() + ' ' + word)
+                    setUsedHints([...usedHints, index])
+                  }}
+                >
+                  {word}
+                </span>
+              ))}
+              <span
+                className={`btn btn-sm btn-outline-dark align-baseline ${
+                  usedHints.length ? 'visible' : 'invisible'
+                }`}
+                style={{ margin: '-5px 0' }}
+                onClick={() => setUsedHints([])}
+              >
+                ×
+              </span>
+            </>
+          ) : (
+            <span className={showAnswer ? undefined : 'invisible'}>
+              {askInGeorgian ? eng : geo}
+            </span>
+          )}
+        </div>
+        <div className="row">
+          <div className="col d-flex justify-content-start">
+            <input
+              type="submit"
+              className="btn btn-primary me-2"
+              value={showAnswer ? 'Next' : 'Check'}
+              disabled={!canSubmit}
+            />
+            <input
+              type="button"
+              className="btn btn-secondary"
+              value="Hint"
+              disabled={showAnswer || hint !== undefined}
+              onClick={onHint}
+            />
           </div>
-        ) : null}
-        <p>
-          <input
-            type="submit"
-            className="btn btn-primary"
-            value={showAnswer ? 'Next' : 'Check'}
-            disabled={!canSubmit}
-          />
-        </p>
+          <div className="col d-flex justify-content-center">
+            <input
+              type="button"
+              className="btn btn-outline-secondary me-2"
+              value="Skip"
+              onClick={() => alert('Not implemented')}
+            />
+            <input
+              type="button"
+              className="btn btn-outline-danger"
+              value="Drop"
+              onClick={() => alert('Not implemented')}
+            />
+          </div>
+          <div className="col d-flex justify-content-end">
+            <input
+              type="button"
+              className="btn btn-warning me-2"
+              value="Hard"
+              onClick={() => alert('Not implemented')}
+            />
+            <input
+              type="button"
+              className="btn btn-success"
+              value="Easy!"
+              onClick={() => alert('Not implemented')}
+            />
+          </div>
+        </div>
       </div>
     </form>
   )

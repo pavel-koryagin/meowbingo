884bc3cee79a590c68e8592702f4163f5a1b6917
Pavel Koryagin <pavel@koryagin.com>
2023-05-25 12:15:57 +0400

    Advance getPronunciation()

--- src/preload/ElectronWrapperAPI.ts
+++ src/preload/ElectronWrapperAPI.ts
@@ -1,5 +1,17 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { getPronunciation } from './getPronunciation'
+
 export interface ElectronWrapperAPI {
   getState: <T>() => Promise<T>
   setState: <T>(state: T) => Promise<void>
   getPronunciation: (sentence: string) => Promise<string | null>
+}
+
+const MEOW_STATE_FILE = process.env.MEOW_STATE_FILE ?? 'data/state.json'
+
+// Custom APIs for renderer
+export const api: ElectronWrapperAPI = {
+  getState: async () => JSON.parse(await readFile(MEOW_STATE_FILE, 'utf-8')),
+  setState: async (state) => writeFile(MEOW_STATE_FILE, JSON.stringify(state, null, 2), 'utf-8'),
+  getPronunciation
 }
